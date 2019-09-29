e2b4eb017474e75aa89ce4a4c0f7cb4e73913539
Mihailus2000 <Mihail14112000@mail.ru>
2019-09-29 23:54:14 +0300

    remove build directories

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mihailus/Mihailus2000/MGTU_2_1_АЯ/lab-01-parser
-# Build directory: /home/mihailus/Mihailus2000/MGTU_2_1_АЯ/lab-01-parser/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(unit_tests "/home/mihailus/Mihailus2000/MGTU_2_1_АЯ/lab-01-parser/cmake-build-debug/tests")
-set_tests_properties(unit_tests PROPERTIES  _BACKTRACE_TRIPLES "/home/mihailus/Mihailus2000/MGTU_2_1_АЯ/lab-01-parser/CMakeLists.txt;91;add_test;/home/mihailus/Mihailus2000/MGTU_2_1_АЯ/lab-01-parser/CMakeLists.txt;0;")
