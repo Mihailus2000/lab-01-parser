46d69a754ea6373353ae154a9ace42f485f0e087
Mihailus2000 <Mihail14112000@mail.ru>
2019-09-30 17:08:36 +0300

    try to add  all in rep to remote

---
+++ cmake-build-debug/CMakeFiles/tests.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tests.dir/tests/testArray.cpp.o"
+  "CMakeFiles/tests.dir/tests/testObject.cpp.o"
+  "CMakeFiles/tests.dir/tests/testJson.cpp.o"
+  "tests.pdb"
+  "tests"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
